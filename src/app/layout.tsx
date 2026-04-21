f2caf79e59f168a4334e3cd1325df8dcf7f1524e
soultani1 <soultani12@gmail.com>
2026-04-21 15:00:13 +0200

    chore: integrate google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -42,6 +43,7 @@ export default function RootLayout({
         <Header />
         <main className="flex-1 pt-[73px]">{children}</main>
         <Footer />
+        <GoogleAnalytics gaId="G-2XETC7DMB1" />
       </body>
     </html>
   );
